2f8a25bd2320ff3c88492787c30d5bc3ea0f24ff
Immaannn2222 <imennaayari@gmail.com>
2020-06-26 16:03:13 +0100

    d

--- models/base_model.py
+++ models/base_model.py
@@ -7,20 +7,32 @@ from datetime import datetime
 
 class BaseModel():
     """ """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            for key, val in kwargs.items():
+                if (key == "created_at" or key == "updated_at"):
+                    val = datetime.strptime(val,
+                                            "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
     def __str__(self):
         """ """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__ )
+        return "[{}] ({}) {}".format(
+            type(self).__name__,
+            self.id,
+            self.__dict__)
 
     def save(self):
         """ """
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         """ """
         dct = self.__dict__

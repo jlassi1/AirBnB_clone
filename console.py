d59509b4d05e8bbb713d8753f0eecdd1deab7fae
Immaannn2222 <imennaayari@gmail.com>
2020-06-29 11:48:41 +0100

    fix console

--- console.py
+++ console.py
@@ -5,19 +5,19 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """ """
+    """command interpreter"""
     prompt = "(hbnb) "
 
     def do_EOF(self, line):
-        """ """
+        """exit the program"""
         return True
 
     def do_quit(self, line):
-        """ """
+        """same as previous"""
         return True
 
     def emptyline(self):
-        """ """
+        """does nothing"""
         pass
 
 

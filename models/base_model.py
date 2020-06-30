4cfb3c07e87fef20b71a7f24050ec0b604ee0518
jlassi1 <khawlajlassi1990@gmail.com>
2020-06-30 14:24:46 +0100

    j

--- models/base_model.py
+++ models/base_model.py
@@ -16,10 +16,11 @@ class BaseModel():
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.now()
             for key, val in kwargs.items():
-                if (key == "created_at" or key == "updated_at"):
-                    val = datetime.strptime(val,
-                                            "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, val)
+                if key is not "__class__":
+                    if (key == "created_at" or key == "updated_at"):
+                        val = datetime.strptime(val,
+                                                "%Y-%m-%dT%H:%M:%S.%f")
+                        setattr(self, key, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()

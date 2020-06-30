0effa7db657e2b3252ec86a4b24b35c9f2c150db
Immaannn2222 <imennaayari@gmail.com>
2020-06-30 14:42:17 +0100

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel():
     """ Base class"""
     def __init__(self, *args, **kwargs):
         """ Initialization """
-        if kwargs is not None:
+        if kwargs is not {}:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()

a9d00156bfc1aaacde7461110d787bc4691d386a
jlassi1 <khawlajlassi1990@gmail.com>
2020-07-01 14:17:04 +0100

    kjjh

--- models/base_model.py
+++ models/base_model.py
@@ -14,14 +14,12 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if kwargs:
-            for key, val in kwargs.items():  
-                if key == "created_at" or key == "updated_at":
-                    val = datetime.strptime(val,
-                                            "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == '__class__':
-                    pass
-                else:
+        if kwargs is not {}:
+            for key, val in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        val = datetime.strptime(val,
+                                                "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, val)
         else:
             models.storage.new(self)

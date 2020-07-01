7a0c916248de3c0d28d5fd85e48c4e691c014c2f
jlassi1 <khawlajlassi1990@gmail.com>
2020-07-01 16:17:33 +0100

    jhez

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -32,7 +32,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(self.my_model.id, str)
         self.assertIsInstance(self.my_model.name, str)
         self.assertIsInstance(self.my_model.my_number, int)
-        #self.assertIsInstance(self.my_model.created_at, str)
+        self.assertIsInstance(self.my_model.created_at, str)
         self.assertEqual(type(self.my_new_model.created_at), datetime)
 
     def test_BaseModel_methods(self):
